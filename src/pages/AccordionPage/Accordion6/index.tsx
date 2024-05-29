eddb7baca892b85284ae7eebe54304b1a39e8025
신준혁 <oridori2705@naver.com>
2024-05-30 00:27:48 +0900

    refactor: 불필요한 import 제거

--- src/pages/AccordionPage/Accordion6/index.tsx
+++ src/pages/AccordionPage/Accordion6/index.tsx
@@ -2,7 +2,6 @@ import { useEffect, useRef, useState } from 'react'
 import data from '../data'
 import {
   AccordionContainer,
-  AccordionDescription,
   AccordionItemLi,
   AccordionTab
 } from './Accordion.styled'
