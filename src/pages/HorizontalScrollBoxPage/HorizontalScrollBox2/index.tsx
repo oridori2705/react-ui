f96e1fd047d832d418fa7c781c329ba9f2294f47
신준혁 <oridori2705@naver.com>
2024-10-16 18:51:15 +0900

    style: 횡 스크롤 박스 두 번째 방법 설명 추가

--- src/pages/HorizontalScrollBoxPage/HorizontalScrollBox2/index.tsx
+++ src/pages/HorizontalScrollBoxPage/HorizontalScrollBox2/index.tsx
@@ -1,6 +1,12 @@
 import LazyImageComponent3 from '@/pages/LazyImagePage/LazyImage3/LazyComponent3'
 import ScrollBox from './ScrollBoxComponent2'
 import data from '../data'
+import UiExplanation from '@/components/UiExplanation'
+import {
+  StyledCode,
+  StyledStrong,
+  StyledStrongPositive
+} from '@/components/UiExplanation/UiExplanation.styled'
 
 const HorizontalScrollBox2 = () => {
   return (
@@ -23,6 +29,51 @@ const HorizontalScrollBox2 = () => {
           </ScrollBox.Wrapper>
         ))}
       </ScrollBox>
+
+      <UiExplanation>
+        <p>
+          - 횡 스크롤 박스 첫 번째 방법에서 내부의 요소에 스타일이나 onClick
+          등의 기능을 보다
+          <StyledStrongPositive>
+            자유롭게 적용할 수 있도록 합성 컴포넌트 패턴
+          </StyledStrongPositive>
+          으로 리팩토링했습니다.
+        </p>
+        <p>
+          - <StyledStrong>스크롤바를 숨김/보임 처리</StyledStrong>할 수 있도록
+          <StyledCode>isSetScrollBar</StyledCode>를 Props로 받을 수 있게
+          했습니다.
+        </p>
+        <br />
+        <p>
+          <StyledStrongPositive>사용 방법</StyledStrongPositive>
+        </p>
+        <p>
+          - 해당 기능은{' '}
+          <StyledCode>
+            {'<'}ScrollBox{'>'}
+          </StyledCode>
+          하위에 꼭{' '}
+          <StyledCode>
+            {'<'}ScrollBox.Wrapper
+            {'>'}
+          </StyledCode>
+          를 사용해서 데이터를 나열해야합니다.
+        </p>
+        <p>
+          - <StyledCode>index Props</StyledCode>를 내려줘서 각 아이템이
+          구분되도록 해야합니다.(순수한 index값이면 됨)
+        </p>
+        <StyledCode>
+          {`<ScrollBox>
+  {data.map((item, index) => (
+    <ScrollBox.Wrapper index={index}>
+      {데이터 나열..}
+    </ScrollBox.Wrapper>
+  ))}
+</ScrollBox>`}
+        </StyledCode>
+      </UiExplanation>
     </>
   )
 }
