88f9ace534715bba307a8ea80f78a78d8585953d
신준혁 <oridori2705@naver.com>
2024-12-23 01:20:21 +0900

    docs: Carousel1,2,3 설명 추가

--- src/pages/CarouselPage/Carousel2/index.tsx
+++ src/pages/CarouselPage/Carousel2/index.tsx
@@ -1,8 +1,30 @@
+import UiExplanation from '@/components/UiExplanation'
 import data from '../data'
 import CarouselComponent2 from './CarouselComponent2'
 
 const Carousel2 = () => {
-  return <CarouselComponent2 images={data}></CarouselComponent2>
+  return (
+    <div>
+      <CarouselComponent2 images={data}></CarouselComponent2>
+      <UiExplanation>
+        <p>
+          - 모든 슬라이드 아이템이 나열되어있는 상태에서 currentIndex를 이용해
+          슬라이드할 때 translateX를 이용해 슬라이드하는 방식입니다.
+        </p>
+        <p>- 이때 3D 효과가 추가되었습니다.</p>
+        <p>
+          - 이미지 너비와는 상관없이 슬라이드 거리 값이 계산되므로 편리합니다.
+        </p>
+        <br />
+        <h3>단점</h3>
+        <p>- 모바일 환경에서 터치 슬라이드는 되지 않습니다.</p>
+        <p>
+          - 만약 이미지 너비가 600px라면 그에 맞는 공간이 주어져야
+          합니다.(그렇지않으면 배치가 올바르게 되지 않음)
+        </p>
+      </UiExplanation>
+    </div>
+  )
 }
 
 export default Carousel2
--- src/pages/CarouselPage/Carousel3/indext.tsx
+++ src/pages/CarouselPage/Carousel3/indext.tsx
@@ -1,3 +1,4 @@
+import UiExplanation from '@/components/UiExplanation'
 import data from '../data'
 import CarouselComponent3 from './CarouselComponent3'
 
@@ -17,6 +18,21 @@ const Carousel3 = () => {
           />
         ))}
       </CarouselComponent3>
+      <UiExplanation>
+        <p>
+          - ChildrenNode에 접근해서 scroll-snap-align 속성, 크기 등을 강제로
+          적용하고, 클릭 + 터치 이벤트를 추가한 방식입니다.
+        </p>
+        <p>- 기존 방식들보다 범용성이 높습니다.</p>
+        <p>- 터치 슬라이드가 가능합니다.</p>
+        <br />
+        <h3>단점</h3>
+        <p>
+          - Carousel은 계속해서 회전해야하는데 현재는 하나의 리스트만
+          나열됩니다.
+        </p>
+        <p>- 이후 방식에서 이어 붙이는 방식을 이용해 개선해보려고 합니다.</p>
+      </UiExplanation>
     </div>
   )
 }
